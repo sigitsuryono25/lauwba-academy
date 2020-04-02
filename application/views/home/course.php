7391cc36ebbd433ef884df32652370d678d13a9f
Sigit Suryono <sigit_suryono25>
2020-04-02 22:38:42 +0700

    initial commit

---
+++ application/views/home/course.php
@@ -0,0 +1,280 @@
+<div class="bg-white pt-4">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 ">
+                <nav>
+                    <div class="nav nav-tabs nav-fill" id="nav-tab" role="tablist">
+                        <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Home</a>
+                        <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Profile</a>
+                        <a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false">Contact</a>
+                        <a class="nav-item nav-link" id="nav-about-tab" data-toggle="tab" href="#nav-about" role="tab" aria-controls="nav-about" aria-selected="false">About</a>
+                    </div>
+                </nav>
+                <div class="tab-content py-3 px-3 px-sm-0" id="nav-tabContent">
+                    <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
+                        Et et consectetur ipsum labore excepteur est proident excepteur ad velit occaecat qui minim occaecat veniam. Fugiat veniam incididunt anim aliqua enim pariatur veniam sunt est aute sit dolor anim. Velit non irure adipisicing aliqua ullamco irure incididunt irure non esse consectetur nostrud minim non minim occaecat. Amet duis do nisi duis veniam non est eiusmod tempor incididunt tempor dolor ipsum in qui sit. Exercitation mollit sit culpa nisi culpa non adipisicing reprehenderit do dolore. Duis reprehenderit occaecat anim ullamco ad duis occaecat ex.
+                    </div>
+                    <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
+                        Et et consectetur ipsum labore excepteur est proident excepteur ad velit occaecat qui minim occaecat veniam. Fugiat veniam incididunt anim aliqua enim pariatur veniam sunt est aute sit dolor anim. Velit non irure adipisicing aliqua ullamco irure incididunt irure non esse consectetur nostrud minim non minim occaecat. Amet duis do nisi duis veniam non est eiusmod tempor incididunt tempor dolor ipsum in qui sit. Exercitation mollit sit culpa nisi culpa non adipisicing reprehenderit do dolore. Duis reprehenderit occaecat anim ullamco ad duis occaecat ex.
+                    </div>
+                    <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">
+                        Et et consectetur ipsum labore excepteur est proident excepteur ad velit occaecat qui minim occaecat veniam. Fugiat veniam incididunt anim aliqua enim pariatur veniam sunt est aute sit dolor anim. Velit non irure adipisicing aliqua ullamco irure incididunt irure non esse consectetur nostrud minim non minim occaecat. Amet duis do nisi duis veniam non est eiusmod tempor incididunt tempor dolor ipsum in qui sit. Exercitation mollit sit culpa nisi culpa non adipisicing reprehenderit do dolore. Duis reprehenderit occaecat anim ullamco ad duis occaecat ex.
+                    </div>
+                    <div class="tab-pane fade" id="nav-about" role="tabpanel" aria-labelledby="nav-about-tab">
+                        Et et consectetur ipsum labore excepteur est proident excepteur ad velit occaecat qui minim occaecat veniam. Fugiat veniam incididunt anim aliqua enim pariatur veniam sunt est aute sit dolor anim. Velit non irure adipisicing aliqua ullamco irure incididunt irure non esse consectetur nostrud minim non minim occaecat. Amet duis do nisi duis veniam non est eiusmod tempor incididunt tempor dolor ipsum in qui sit. Exercitation mollit sit culpa nisi culpa non adipisicing reprehenderit do dolore. Duis reprehenderit occaecat anim ullamco ad duis occaecat ex.
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+<div class="mt-2 mb-2 py-4" style="padding: 0px 0px 0px 0px">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-2 mt-2">
+                                <img class="img-fluid" src="https://i.udemycdn.com/browse_components/value-props-unit/go_own_pace.svg">
+                            </div>
+                            <div class="col-md-10">
+                                <h5>Belajar sesuai kecepatan Anda</h5><br/>
+                            </div>
+                            <div class="col-md-10 offset-2">
+                                <span class="text-justify">Nikmati akses seumur hidup ke kursus di Lauwba Academy<br/><br/></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-2 mt-2">
+                                <img class="img-fluid" src="https://i.udemycdn.com/browse_components/value-props-unit/learn_from_experts.svg">
+                            </div>
+                            <div class="col-md-10">
+                                <h5>Belajar dari Trainer yang Handal</h5><br/>
+                            </div>
+                            <div class="col-md-10 offset-2">
+                                <span class="text-justify">Kami memiliki Trainer yang handal dan tersertifikasi<br/><br/></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-2 mt-2">
+                                <img class="img-fluid" src="https://i.udemycdn.com/browse_components/value-props-unit/video_courses.svg">
+                            </div>
+                            <div class="col-md-10">
+                                <h5>Temukan kursus video yang sesuai dengan minat dan kemampuan anda</h5><br/>
+                            </div>
+                            <div class="col-md-10 offset-2">
+                                <span class="text-justify">Bangun perpustakaan untuk karier dan pertumbuhan pribadi Anda</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="py-5  text-center text-white bg-dark" >
+    <div class="container py-5 my-5">
+        <div class="row">
+            <div class="mx-auto col-lg-5 col-md-7 col-10 text-white">
+                <h4>Dapatkan rekomendasi yang dipersonalisasi</h4>
+                <p class="mb-3 text-white">Jawab beberapa pertanyaan untuk pilihan terbaik Anda</p>
+                <a class="btn btn-primary" href="#">Mulai Sekarang</a>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="py-5 bg-white">
+    <div class="container">
+        <h4 class="mb-2">Kategori Yang Tersedia</h4>
+        <div class="row">
+            <div class="col-md-3 mt-3">
+                <div class="card">
+                    <div class="card-body   shadow rounded ">
+                        <div class="row">
+                            <div class="col-md-3">
+                                <i class="fa fa-3x fa-android text-success"></i>
+                            </div>
+                            <div class="col-md-9">
+                                <p class="mt-2">Android</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-3 mt-3">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-3">
+                                <i class="fa fa-3x fa-globe" style="color: orange"></i>
+                            </div>
+                            <div class="col-md-9">
+                                <p class="mt-2">Website</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 mt-3">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-3">
+                                 <i class="fa fa-3x fa-laptop text-info"></i>
+                            </div>
+                            <div class="col-md-9">
+                                <p class="mt-2">Teknisi Komputer</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-3 mt-3">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-3">
+                                 <i class="fa fa-3x fa-apple text-dark"></i>
+                            </div>
+                            <div class="col-md-9">
+                                <p class="mt-2">iOS</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 mt-3">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-3">
+<!--                                <img class="img-fluid" src="https://i.udemycdn.com/browse_components/value-props-unit/video_courses.svg">-->
+                                <i class="fa fa-3x fa-paypal text-primary"></i>
+                            </div>
+                            <div class="col-md-9">
+                                <p class="mt-2">Digital Marketing</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 mt-3">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-3">
+                                <i class="fa fa-3x fa-google text-danger"></i>
+                            </div>
+                            <div class="col-md-9">
+                                <p class="mt-2">SEO</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="py-5">
+    <div class="container">
+        <div class="row">
+            <div class="mx-auto col-md-12">
+                <h4 class="mb-2">Apa Kata Peserta Kami?</h4>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-4 col-md-6 p-3">
+                <div class="card">
+                    <div class="card-body p-4">
+                        <div class="row">
+                            <div class="col-md-4 col-3"> <img class="img-fluid d-block rounded-circle" src="https://static.pingendo.com/img-placeholder-2.svg"> </div>
+                            <div class="d-flex  col-md-8 flex-column justify-content-center align-items-start col-9">
+                                <p class="mb-0 lead"> <b>J. W. Goethe</b> </p>
+                                <p class="mb-0">Co-founder</p>
+                            </div>
+                        </div>
+                        <p class="mt-3 mb-0">I throw myself down among the tall grass by the trickling stream; and, as I lie close to the earth, a thousand unknown plants are noticed by me: when I hear the buzz of the little world.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 p-3">
+                <div class="card">
+                    <div class="card-body p-4">
+                        <div class="row">
+                            <div class="col-md-4 col-3"> <img class="img-fluid d-block rounded-circle" src="https://static.pingendo.com/img-placeholder-1.svg"> </div>
+                            <div class="d-flex  col-md-8 flex-column justify-content-center align-items-start col-9">
+                                <p class="mb-0 lead"> <b>G. W. John</b> </p>
+                                <p class="mb-0">CEO &amp; founder</p>
+                            </div>
+                        </div>
+                        <p class="mt-3 mb-0">I lie close to the earth, a thousand unknown plants are noticed by me: when I hear the buzz of the little world among the stalks, and grow familiar with the countless indescribable forms of the insects and flies.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 p-3">
+                <div class="card">
+                    <div class="card-body p-4">
+                        <div class="row">
+                            <div class="col-md-4 col-3"> <img class="img-fluid d-block rounded-circle" src="https://static.pingendo.com/img-placeholder-3.svg"> </div>
+                            <div class="d-flex  col-md-8 flex-column justify-content-center align-items-start col-9">
+                                <p class="mb-0 lead"> <b>J. G. Wolf</b> </p>
+                                <p class="mb-0">CFO</p>
+                            </div>
+                        </div>
+                        <p class="mt-3 mb-0">Oh, would I could describe these conceptions, could impress upon paper all that is living so full and warm within me, that it might be the mirror of my soul, as my soul is the mirror of the infinite God!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="pb-5 text-center" >
+    <div class="container">
+        <span class="mb-3">Dipercayai Oleh</span><br><br>
+        <div class="row text-center">
+            <div class="mx-auto col-md-7">
+                <div class="row text-muted">
+                    <div class="col-md-3 col-4 p-2 mb-2"> <i class="d-block fa fa-angellist fa-3x"></i> </div>
+                    <div class="col-md-3 col-4 p-2 mb-2"> <i class="d-block fa fa-cc-visa fa-3x"></i> </div>
+                    <div class="col-md-3 col-4 p-2 mb-2"> <i class="d-block fa fa-amazon fa-3x"></i> </div>
+                    <div class="col-md-3 col-4 p-2 mb-2"> <i class="d-block fa fa-paypal fa-3x"></i> </div>
+                    <div class="col-4 p-2 mb-2"> <i class="d-block fa fa-empire fa-3x"></i> </div>
+                    <div class="col-4 p-2 mb-2"> <i class="d-block fa fa-rebel fa-3x"></i> </div>
+                    <div class="col-4 p-2 mb-2"> <i class="d-block fa fa-first-order fa-3x"></i> </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="py-3 text-center" >
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-5 col-md-6 p-4 border-right"> 
+                <h4><b>Daftar Sekarang</b></h4><br/>
+                <p>Dapatkan akses gratis selamanya untuk semua modul dan video yang telah anda bayarkan</p>
+            </div>
+            <div class="col-lg-5 col-md-6 p-4"> 
+                <h4> <b>Lauwba Academy for Business</b> </h4><br/>
+                <p>Daftarkan Instansi/Kelompok Anda untuk mendapatkan Bimbingan Teknis dari tim kami</p>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ application/views/template/home.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <title>Aasana Fitness category Flat Bootstrap Responsive website Template | Aasana :: w3layouts</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta charset="utf-8" />
+        <script>
+            addEventListener("load", function () {
+                setTimeout(hideURLbar, 0);
+            }, false);
+
+            function hideURLbar() {
+                window.scrollTo(0, 1);
+            }
+        </script>
+        <link href="<?php echo base_url('assets/') ?>css/bootstrap.css" type="text/css" rel="stylesheet" media="all">
+        <link href="<?php echo base_url('assets/') ?>css/aasana.css" type="text/css" rel="stylesheet" media="all">
+        <link href="<?php echo base_url('assets/') ?>css/font-awesome.min.css" rel="stylesheet">
+        <link href="//fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i" rel="stylesheet">
+    </head>
+    <style>
+        .dropdown:hover>.dropdown-menu {
+            display: block;
+        }
+
+        .dropdown>.dropdown-toggle:active {
+            /*Without this, clicking will make it sticky*/
+            pointer-events: none;
+        }
+    </style>
+    <body>
+        <!--BANNER-->
+        <?php echo $banner ?>
+
+        <div class="container-fluid p-0 bg-light">
+            <!--content start here-->
+            <?php echo $content?>
+        </div>
+
+        <!-- footer -->
+        <div class="footerv4-w3ls" id="footer">
+            <div class="footerv4-top">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-3 col-sm-12 footv4-left">
+                            <h3>About Us</h3>
+                            <h4 class="text-white">
+                                <a class="text-white" href="<?php echo site_url() ?>">Lauwba Academy</a>
+                            </h4>
+                            <p class="text-white"></p>
+                            <div class="footerv4-social">
+                                <h3>stay connected</h3>
+                                <ul>
+                                    <li>
+                                        <a href="https://fb.com/LauwbaTechno">
+                                            <span class="fa fa-facebook"></span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <span class="fa fa-instagram"></span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <span class="fa fa-twitter"></span>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <!-- //footer social -->
+
+                        </div>
+                        <div class="col-lg-3 offset-lg-6 col-sm-1 footv4-content mt-sm-0 mt-4">
+                            <h3>featured content</h3>
+                            <ul class="v4-content">
+                                <li>
+                                    <a href="<?php echo site_url() ?>">Home</a>
+                                </li>
+                                <li>
+                                    <a href="#about" class="scroll">Kategori</a>
+                                </li>
+                                <li>
+                                    <a href="https://www.lauwba.com/" class="font-weight-bold">Lauwba Techno Indonesia</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <!-- /footerv4-top -->
+            </div>
+        </div>
+        <!-- //footer -->
+        <div class="cpy-right">
+            <p>&copy; <?php echo date('Y') ?> Lauwba Academy Design By LTI Creative Team</p>
+        </div>
+
+        <script src="<?php echo base_url('assets/') ?>js/jquery-2.2.3.min.js"></script>
+        <script src="<?php echo base_url('assets/') ?>js/responsiveslides.min.js"></script>
+        <script>
+            // You can also use"$(window).load(function() {"
+            $(function () {
+                // Slideshow 4
+                $(".slider3").responsiveSlides({
+                    auto: true,
+                    pager: true,
+                    nav: false,
+                    speed: 500,
+                    namespace: "callbacks",
+                    before: function () {
+                        $('.events').append("<li>before event fired.</li>");
+                    },
+                    after: function () {
+                        $('.events').append("<li>after event fired.</li>");
+                    }
+                });
+
+            });
+        </script>
+        <script src="<?php echo base_url('assets/') ?>js/move-top.js"></script>
+        <script src="<?php echo base_url('assets/') ?>js/easing.js"></script>
+        <script>
+            jQuery(document).ready(function ($) {
+                $(".scroll").click(function (event) {
+                    event.preventDefault();
+
+                    $('html,body').animate({
+                        scrollTop: $(this.hash).offset().top
+                    }, 1000);
+                });
+            });
+        </script>
+        <script>
+            $(document).ready(function () {
+                $().UItoTop({
+                    easingType: 'easeOutQuart'
+                });
+
+            });
+        </script>
+        <script src="<?php echo base_url('assets/') ?>js/SmoothScroll.min.js"></script>
+        <script src="<?php echo base_url('assets/') ?>js/counter.js"></script>
+        <script src="<?php echo base_url('assets/') ?>js/bootstrap.js"></script>
+    </body>
+
+</html>
