5ef605bd47aad380142bc8c656a7ab3079b2b0cb
Sigit Suryono <sigit_suryono25@hotmail.com>
2020-04-04 22:24:23 +0700

    Update template.php

--- hacked-usa/application/views/template.php
+++ hacked-usa/application/views/template.php
@@ -8,7 +8,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content />
         <meta name="author" content />
-        <title>Blank - SB Admin Pro</title>
+        <title>Trainer Porta | Lauwba Academy</title>
         <link href="<?php echo base_url('assets/') ?>css/styles.css" rel="stylesheet" />
         <link rel="icon" type="image/x-icon" href="assets/img/favicon.png" />        
         <link href="<?php echo base_url('assets/') ?>js/datatables/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet" />
